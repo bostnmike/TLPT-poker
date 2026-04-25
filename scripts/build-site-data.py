aad1dd6eec148fe1ce89e800d313f9c793e6a294
bostnmike <blanchettemr@gmail.com>
2026-04-25 16:29:56 -0400

    Add RAW_FIELD_MAP for site data mapping

---
+++ scripts/build-site-data.py
@@ -0,0 +1,11 @@
+RAW_FIELD_MAP = {
+    "buyIns": "Buy Ins",
+    "rebuys": "Rebuys",
+    "entries": "Entries",
+    "hits": "Hits",
+    "timesPlaced": "Times Placed",
+    "bubbles": "Bubbles",
+    "totalCost": "Total Cost",
+    "totalWinnings": "Total Winnings",
+    "profit": "Total Take"
+}
